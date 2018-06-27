12c3d250523b10f273a2caf18141ff6a0f187bc0
Calebe Rios <calebe_rios@hotmail.com>
2018-06-26 22:46:49 -0300

    Creating test for adding new_task and delete_task

---
+++ test_taskbot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from taskbot import *
+
+class TestTaskBot(unittest.TestCase):
+    id = '1'
+
+    def test_anew_task(self):
+        command = TasksController.new_task('TESTE', '12234')
+        self.id = command[18]
+        self.assertEqual(command, 'New task *TODO* [[{}]] TESTE'.format(self.id))
+
+    def test_delete_task(self):
+        command = TasksController.delete_task(self.id, '12234')
+        self.assertEqual(command, 'Task [[{}]] deleted'.format(self.id))
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestTaskBot)
+unittest.TextTestRunner(verbosity=2).run(suite)
